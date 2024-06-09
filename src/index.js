a81045800cce5bd3b708ada142fd4186b150a334
SINFA - 07.22 Dpto. de Ingenieria del Software (SO2.FAP Escobar Quintana Luis Guiseppe) <lescobar@fap.mil.pe>
2024-06-09 17:39:12 -0500

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apicache = require("apicache");
+const cors = require("cors");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cache = apicache.middleware;
 
+app.use(cors());
 app.use(express.json());
 //app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
